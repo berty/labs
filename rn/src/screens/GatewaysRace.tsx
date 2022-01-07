52b11887bf1c0b1e0ee36f7dddd5e24d7dcf1c39
Norman Meier <norman@berty.tech>
2022-01-07 02:17:54 +0100

    fix: groom gateways race
    Signed-off-by: Norman Meier <norman@berty.tech>

--- rn/src/screens/GatewaysRace.tsx
+++ rn/src/screens/GatewaysRace.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { ImageStyle, SafeAreaView, ScrollView, ViewStyle } from 'react-native'
 
 import { ScreenFC } from '@berty-labs/navigation'
 import { defaultColors } from '@berty-labs/styles'
@@ -12,13 +12,32 @@ import { useGomobileIPFS } from '@berty-labs/ipfsutil'
 const superlativeApe = '/ipfs/QmQVS8VrY9FFpUWKitGhFzzx3xGixAd4frf1Czr7AQxeTc/1.png'
 const externalGatewayURL = 'https://gateway.pinata.cloud'
 
-const style = { width: 42, height: 42 }
+const style: ViewStyle & ImageStyle = {
+	width: '60%',
+	aspectRatio: 1,
+	alignItems: 'center',
+	justifyContent: 'center',
+}
 
-export const IPFSDemo: React.FC = () => {
+const prettyMilliSeconds = (ms: number) => {
+	if (ms >= 60000) {
+		return `${ms / 60000}min`
+	}
+	if (ms >= 1000) {
+		return `${ms / 1000}sec`
+	}
+	return `${ms}ms`
+}
+
+export const GatewaysRace: ScreenFC<'GatewaysRace'> = () => {
 	const isDarkMode = useColorScheme() === 'dark'
+	const init = React.useRef(Date.now())
+	const [loadedLocal, setLoadedLocal] = React.useState<number>()
+	const [loadedExiternal, setLoadedExternal] = React.useState<number>()
 	const mobileIPFS = useGomobileIPFS()
 	const textStyle = {
 		color: isDarkMode ? Colors.white : Colors.black,
+		fontSize: 18,
 	}
 	const [imageURI, setImageURI] = React.useState<string>()
 	React.useEffect(() => {
@@ -50,32 +69,52 @@ export const IPFSDemo: React.FC = () => {
 		return <Text style={textStyle}>IPFS: {mobileIPFS.state}</Text>
 	}
 	return (
-		<View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 30 }}>
-			<View>
-				<Text style={textStyle}>External gateway</Text>
-				<Image style={style} source={{ uri: externalGatewayURL + superlativeApe }} />
-			</View>
-			<View>
-				<Text style={textStyle}>Gomobile gateway</Text>
-				{imageURI ? (
-					<Image
-						style={style}
-						source={{ uri: imageURI }}
-						onError={({ nativeEvent: { error } }) => console.warn(error)}
-						onLoad={() => console.log('local image loaded')}
-					/>
-				) : (
-					<ActivityIndicator />
-				)}
-			</View>
-		</View>
-	)
-}
-
-export const GatewaysRace: ScreenFC<'GatewaysRace'> = () => {
-	return (
 		<SafeAreaView style={{ backgroundColor: defaultColors.background, flex: 1 }}>
-			<IPFSDemo />
+			<ScrollView>
+				<View style={{ marginVertical: 30 }}>
+					<View style={{ alignItems: 'center', paddingHorizontal: 30 }}>
+						<Text style={{ color: defaultColors.white, opacity: 0.7 }}>
+							This test loads a react-native Image from the embedded Gomobile-IPFS gateway and the
+							Pinata gateway concurrently
+						</Text>
+					</View>
+					<View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 30 }}>
+						<View style={{ width: '50%', alignItems: 'center' }}>
+							<Text style={[textStyle, { marginBottom: 30 }]}>External gateway</Text>
+							<Image
+								style={style}
+								source={{ uri: externalGatewayURL + superlativeApe }}
+								onLoad={() => setLoadedExternal(Date.now())}
+							/>
+							{loadedExiternal && (
+								<Text style={[textStyle, { marginTop: 15 }]}>
+									{prettyMilliSeconds(loadedExiternal - init.current)}
+								</Text>
+							)}
+						</View>
+						<View style={{ width: '50%', alignItems: 'center' }}>
+							<Text style={[textStyle, { marginBottom: 30 }]}>Embedded gateway</Text>
+							{imageURI ? (
+								<Image
+									style={style}
+									source={{ uri: imageURI }}
+									onError={({ nativeEvent: { error } }) => console.warn(error)}
+									onLoad={() => setLoadedLocal(Date.now())}
+								/>
+							) : (
+								<View style={style}>
+									<ActivityIndicator />
+								</View>
+							)}
+							{loadedLocal && (
+								<Text style={[textStyle, { marginTop: 15 }]}>
+									{prettyMilliSeconds(loadedLocal - init.current)}
+								</Text>
+							)}
+						</View>
+					</View>
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	)
 }
